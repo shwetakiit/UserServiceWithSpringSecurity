99b5919c73d93c03d73ecd52b9bff635320729fc
shwetakiit <shweta0729e2open@gmail.com>
2024-05-25 11:04:27 +0530

    Implementing User service Self

---
+++ src/main/java/kumari/shweta/userservice/userservice/models/User.java
@@ -0,0 +1,23 @@
+package kumari.shweta.userservice.userservice.models;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class User extends BaseModel {
+    private String name;
+    private String hashedpassword;
+    private String email;
+    @ManyToMany
+    private List<Role> roles;
+    private  boolean isEmailVerified;
+}
